6880ccaf92cfbd689efddf717031b6d91012a219
Callum Lees <dev.cal.lees@gmail.com>
2022-09-05 22:18:01 +0100

    Created Navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-blue-800 sticky top-0 z-10">
+        <div class="flex mx-auto max-w-3xl py-10 bg-blue-800 text-center">
+            <!-- Font is big for testing purposes -->
+            <nuxt-link to="/" class="text-5xl text-white hover:text-indigo-600 transition duration-300">Home</nuxt-link>
+            <nuxt-link to="/about" class="text-5xl text-white pl-6 hover:text-indigo-600 transition duration-300">About</nuxt-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
