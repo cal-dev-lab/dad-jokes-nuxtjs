cbed4c7d7271ca0d51013b1c267783995c9a39ab
Callum Lees <dev.cal.lees@gmail.com>
2022-09-05 22:19:18 +0100

    Added meta data info

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <Navbar />
+        <nuxt />
+    </div>
+    
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+export default {
+    head() {
+        return {
+            meta: [
+                {
+                    hid: "description",
+                    name: "description",
+                    content: "Best place for dad jokes.",
+                }
+            ],
+            link: [
+                // Dynamic canonical URL
+                {
+                    rel: "canonical",
+                    href: "https://illustrious-moxie-b1844f.netlify.app" + this.$route.path
+                }
+            ]
+        };
+    },
+    components: { Navbar }
+}
+</script>
